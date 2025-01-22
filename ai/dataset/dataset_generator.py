f2b9b481bef57806cccc6635a7ca101e8f99ddd2
Pascal Gula <pascal.gula@gmail.com>
2025-01-22 22:59:40 +0100

    final dataset generation including all individual json and images

--- ai/dataset/dataset_generator.py
+++ ai/dataset/dataset_generator.py
@@ -1,14 +1,15 @@
-from typing import integer, List, Tuple
+from typing import List, Tuple
 import random
 import json
 
-from .room_data import Room
-from .room_generator import generate_room, generate_lights, save_room_to_string
+from room_data import Room
+from room_generator import generate_room, generate_lights, save_room_to_string
+from image_generator import generate_room_image
 
 
-def generate_random_size() -> Tuple(integer, integer):
-    length = random.randint(50, 2000)
-    width = random.randint(50, length)
+def generate_random_size() -> Tuple[int, int]:
+    length = random.randint(100, 200)
+    width = random.randint(80, length)
     return length, width
 
 
@@ -18,14 +19,33 @@ def generate_random_type() -> str:
 
 def generate_dataset(num_rooms) -> List[Room]:
     dataset = []
+
     for i in range(num_rooms):
         length, width = generate_random_size()
         room = generate_room(length, width)
         room_type = generate_random_type()
         lights_pattern, lights_layout = generate_lights(room, room_type)
-        room["lights"] = lights_pattern
+        room.lights = lights_pattern
+        generate_room_image(
+            room,
+            f"ai/dataset/generated/room_{i}_{length}_{width}_{room_type}_pattern.png",
+        )
+        with open(
+            f"ai/dataset/generated/room_{i}_{length}_{width}_{room_type}_pattern.json",
+            "w",
+        ) as f:
+            json.dump(room.to_dict(), f)
         room_pattern = save_room_to_string(room)
-        room["lights"] = lights_layout
+        room.lights = lights_layout
+        generate_room_image(
+            room,
+            f"ai/dataset/generated/room_{i}_{length}_{width}_{room_type}_layout.png",
+        )
+        with open(
+            f"ai/dataset/generated/room_{i}_{length}_{width}_{room_type}_layout.json",
+            "w",
+        ) as f:
+            json.dump(room.to_dict(), f)
         room_layout = save_room_to_string(room)
 
         dataset.append(
@@ -42,12 +62,13 @@ def generate_dataset(num_rooms) -> List[Room]:
                 ]
             }
         )
+
     return dataset
 
 
 if __name__ == "__main__":
-    dataset = generate_dataset(2)
+    dataset = generate_dataset(300)
 
     # save dataset to a json file
-    with open("dataset.json", "w") as f:
+    with open("ai/dataset/generated/dataset.json", "w") as f:
         json.dump(dataset, f)
